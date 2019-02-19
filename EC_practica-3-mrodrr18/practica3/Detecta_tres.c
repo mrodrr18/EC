8fe30c80a27af3973dc1e8ec1091977dea097efd
mrodrr18 <mrodrr18@estudiantes.unileon.es>
2019-02-19 12:10:09 +0100

    Evolucion practica 3

---
+++ EC_practica-3-mrodrr18/practica3/Detecta_tres.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	// Este programa indica si el número leído por teclado es 3
+// Declaro las variables de mi función
+int Num;
+//Sustituyo la función ESCRIBA "cadena" por printf
+printf("Escribe un número: ");
+//Sustituyo la función LEA Num por scanf (" %d", &variableEntera);
+scanf(" %d", &Num); //Guarda el número leído en la variable Num
+//Compruebo si el número introducido es el 3
+if (Num == 3){
+//Sustituyo la función ESCRIBA "cadena" por printf
+printf("El número introducido es el 3\n");
+}
+else {
+//Sustituyo la función ESCRIBA "cadena" por printf
+printf("El número introducido no es el 3\n");
+}
+
+return 0;
+
+
+
+}
